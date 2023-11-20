20dd9f7c77222cb8c9ae71228aca5f884377efa3
Diary13 <dr.andriamampitasoa@gmail.com>
2023-11-20 09:51:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="container">
+      <div>
+        <h2>Title</h2>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus natus quisquam dolores, reprehenderit ratione inventore debitis, omnis sapiente magni neque libero atque? Recusandae sint et, officia distinctio molestias rem ullam.</h1>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
